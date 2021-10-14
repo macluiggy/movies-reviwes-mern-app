128911089ed1ae55132f6c8ad6a7df5a54dc052a
macluiggy <ferrinluiggy@gmail.com>
2021-10-14 13:09:25 -0500

    comment moviesDAO

--- backend/dao/moviesDAO.js
+++ backend/dao/moviesDAO.js
@@ -32,7 +32,7 @@ export default class MoviesDAO {
 
 		let cursor //definimos el cursor
 		try {//intentamos
-			cursor = await movies//se espera que objener la colection antes creada en injectDB
+			cursor = await movies//se espera a objener la colection antes creada en injectDB
 						.find(query)//encuntra la/las peliculas que coincidan el el query
 						.limit(moviesPerPage)//limita el resultado
 						.skip(moviesPerPage * page)//omite el numero de peliculas que se encuentra
@@ -41,9 +41,13 @@ export default class MoviesDAO {
 												   //si se usa este con limit, este se aplicara primero
 												   //el limite se aplicara solo a los documentos que
 												   //aparecen despues de la omision
-			const moviesList = await cursor.toArray()
-			const totalNumMovies = await movies.countDocuments(query)
-			return { moviesList, totalNumMovies }
+			const moviesList = await cursor.toArray()//crea un array con el filtro que se le aplico
+												     //al cursor
+			const totalNumMovies = await movies.countDocuments(query)//obten el numero total de peliculas
+																	 //contando el numero de documentos
+																	 //en el query
+			return { moviesList, totalNumMovies }//retorna la lista de peliculas y el numero total de
+												 //peliculas encontradas
 		}
 		catch(e) {
 			console.error(`Unable to issue find command, ${e}`)
