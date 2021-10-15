90615182824ba02e9bfad4d6ff1e7fdb9ec498e4
macluiggy <ferrinluiggy@gmail.com>
2021-10-15 07:45:10 -0500

    adding controller for reviews

--- backend/api/movies.route.js
+++ backend/api/movies.route.js
@@ -1,8 +1,15 @@
 import express from 'express'
 import MoviesController from './movies.controller.js'
+import ReviewsController from './reviews.controller.js'
 
 const router = express.Router() // obten acceso al router de express
 //router.route('/').get((req, res) => res.send('hello world'))
 router.route('/').get(MoviesController.apiGetMovies)
 
+router
+	.route("/review")
+	.post(ReviewsController.apiPostReview)
+	.put(ReviewsController.apiUpdateReview)
+	.delete(ReviewsController.apiDeleteReview)
+	
 export default router
