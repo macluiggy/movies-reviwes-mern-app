2fa75e9cdad5680c965e3d3e1a10f192fd9b018e
macluiggy <ferrinluiggy@gmail.com>
2021-10-19 09:46:44 -0500

    adding routes to switch into links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Switch, Route, Link } from 'react-router-dom';
@@ -12,18 +13,54 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
+   const [user, setUser] = React.useState(null)
+
+   async function login(user = null) {
+     // body...
+     setUser(user)
+   }
+
+   async function logout() {
+     // body...
+     setUser(null)
+   }
+
   return (
     <div className="App">
       <Navbar bg='light' expand='lg'>
-        <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href='#home'>Movie Reviews</Navbar.Brand>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
           <Nav className='mr-auto'>
-            <Nav.Link href='#home'>Home</Nav.Link>
-            <Nav.Link href='#link'>Link</Nav.Link>
+            <Nav.Link>
+              <Link to={'/movies'}>Movies</Link>
+            </Nav.Link>
+            <Nav.Link>
+              { user ? (
+                <a onClick={logout}>Logout User</a>
+              ) : (
+                <Link to={'/login'}>Login</Link>
+              )} 
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
+
+      <Switch>
+        <Route exact path={["/","movies"]} component={MoviesList} ></Route>
+        <Route path='/movies/:id/review' render={(props) =>
+          <AddReview {...props} user={user} />
+        } >
+        </Route>
+        <Route path='/movies/:id' render={(props) =>
+          <Movie {...props} user={user} />
+        } >
+        </Route>
+        <Route path='/login' render={(props) =>
+          <Login {...props} login={login} />
+        } >
+        </Route>
+      </Switch>
     </div>
   );
 }
