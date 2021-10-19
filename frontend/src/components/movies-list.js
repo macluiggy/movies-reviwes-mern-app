65347d2fa41a0ecbcf8652af9bb21bd92a0e3763
macluiggy <ferrinluiggy@gmail.com>
2021-10-19 11:11:09 -0500

    retrieveng movies and ratings

--- frontend/src/components/movies-list.js
+++ frontend/src/components/movies-list.js
@@ -1,10 +1,47 @@
+import { useState, useEffect } from 'react'
+import MovieDataService from '../services/movies'
+import { link } from 'react-router-dom'
 
-function MovieList() {
+/*function MovieList() {
 	return (
 		<div className='App'>
 			Movie List
 		</div>
 	);
+}*/
+
+const MovieList = props => {
+	const [movies, setMovies] = useState([])
+	const [searchTitle, setSearchTitle] = useState('')
+	const [searchRating, setSearchRating] = useState('')
+	const [ratings, setRatings] = useState(['All Ratings'])
+
+	useEffect(() => {
+		retrieveMovies()
+		retrieveRatings()
+	}, [])
+
+	const retrieveMovies = () => {
+		MovieDataService.getAll()
+			.then(response => {
+				console.log(response)
+				setMovies(response.data.movies);
+			})
+			.catch(e => {
+				console.log(e)
+			})
+	}
+
+	const retrieveRatings = () => {
+		MovieDataService.getRatings()
+			.then(response => {
+				console.log(response.data)
+				setRatings(['All Ratings']).concat(response.data)
+			})
+			.catch(e => {
+				console.log(e)
+			})
+	}
 }
 
 export default MovieList;
