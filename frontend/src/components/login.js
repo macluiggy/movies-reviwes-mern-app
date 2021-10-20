7094f195dddfd071e1950f904043f8f5cbb65a46
macluiggy <ferrinluiggy@gmail.com>
2021-10-20 08:45:22 -0500

    adding the login component

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,10 +1,20 @@
+import { useState } from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
-function Login() {
-	return (
-		<div className="App">
-			Login
-		</div>
-	);
+const Login = () => {
+	const[name, setName] = useState('');
+	const [id, setId] = useState('')
+
+	const onChangeName = e => {
+		const name = e.target.value
+		setName(name)
+	}
+
+	const onChangeId = e => {
+		const id = e.target.value;
+		setId(id)
+	}
 }
 
 export default Login;
