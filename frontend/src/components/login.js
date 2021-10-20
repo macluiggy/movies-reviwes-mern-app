58ab2f9790a742476c4bfbfd85c8b88b03a71aa4
macluiggy <ferrinluiggy@gmail.com>
2021-10-20 09:45:56 -0500

    setting the user and its id

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -2,19 +2,53 @@ import { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
-const Login = () => {
+const Login = props => {
 	const[name, setName] = useState('');
 	const [id, setId] = useState('')
-
+	console.log(props.history.push)
 	const onChangeName = e => {
-		const name = e.target.value
+		const name = e.target.value//actualiza el nombre
 		setName(name)
 	}
 
 	const onChangeId = e => {
 		const id = e.target.value;
-		setId(id)
+		setId(id)//actualiza el id
 	}
+
+	const login = () => {
+		//cuando se haga click en submit
+		props.login({ name: name, id: id })//establece el usuario proveniente de props
+		props.history.push('/')//actualiza el path para que se redireccione a la pagina pricipal
+	}
+
+	return (
+		<div>
+			<Form>
+				<Form.Group>
+					<Form.Label>Username</Form.Label>
+					<Form.Control
+				       type='text'
+				       placeholder='Enter username'
+				       value={name}
+				       onChange={onChangeName}		
+					/>
+				</Form.Group>
+				<Form.Group>
+					<Form.Label>ID</Form.Label>
+					<Form.Control 
+					   type='text'
+					   placeholder='Enter id'
+					   value={id}
+					   onChange={onChangeId}
+					/>
+				</Form.Group>
+				<Button variant='primary' onClick={login}>
+					Submit
+				</Button>
+			</Form>
+		</div>
+	)
 }
 
 export default Login;
