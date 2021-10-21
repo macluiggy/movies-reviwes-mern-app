a9ec513b03062697285ffabb2fd03d9abaa7ca41
macluiggy <ferrinluiggy@gmail.com>
2021-10-21 08:47:04 -0500

    eyyegrsabfdafd

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,11 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom hola
 import '@testing-library/jest-dom';
+
+/*
+[build.environment]
+  NODE_VERSION = "14"
+  NPM_FLAGS = "--prefix=/dev/null"
+[build]
+  command = "CI=false && npx pnpm i --store=frontend/node_modules/.pnpm-store && npx pnpm run build"
+*/
